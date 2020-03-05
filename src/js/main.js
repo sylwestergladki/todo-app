b1834a44ace27e86278396ac390b39e5d2f21cbb
sylwestergladki <sylwester.gladki@gmail.com>
2020-03-05 08:31:58 +0100

    feat: JS add task

--- src/js/main.js
+++ src/js/main.js
@@ -18,22 +18,28 @@ if ('serviceWorker' in navigator) {
 
 const addButton = document.getElementById("add-button");
 const inputTask = document.getElementById("input-task");
-const todoTask = document.getElementById("tasks-todo")
-let task;
+const todoTask = document.getElementById("tasks-todo");
+const doneButton = document.getElementById("done-button");
+const todoElement = document.getElementById('todoElement');
+let task="";
 
 
 
 const addTask = () => {
+  
   task = inputTask.value;
-  todoTask.innerHTML += `<div class="container-todo__element">
+  
+
+  if(task){
+  todoTask.innerHTML += `<div class="container-todo__element" id="toodoElement">
   <p class="container-todo__element-text">${task}</p>
   <button class='container-todo__element-undone'>X</button>
-  <button class='container-todo__element-done'>OK</button>
+  <button class='container-todo__element-done' id="done-button">OK</button>
 </div>`
 }
-
-const doneTask = () =>{
-  
+  inputTask.value = "";
 }
 
+
 addButton.addEventListener("click", addTask);
+doneButton.addEventListener("click", doneTask);
