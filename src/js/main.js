1e2b640201255810a1c1ca3a513c677842b1a60b
sylwestergladki <sylwester.gladki@gmail.com>
2020-03-05 11:10:44 +0100

    feat: JS doneTask

--- src/js/main.js
+++ src/js/main.js
@@ -1,26 +1,12 @@
 "use strict";
 
-// service worker registration - remove if you're not going to use it
-
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('serviceworker.js').then(function(registration) {
-      // Registration was successful
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
-      // registration failed :(
-      console.log('ServiceWorker registration failed: ', err);
-    });
-  });
-}
-
-// place your code below
 
 const addButton = document.getElementById("add-button");
 const inputTask = document.getElementById("input-task");
 const todoTask = document.getElementById("tasks-todo");
-const doneButton = document.getElementById("done-button");
-const todoElement = document.getElementById('todoElement');
+const doneButton = document.getElementsByClassName("container-todo__element-done");
+const elementToDO = document.getElementById("todoElement");
+
 let task="";
 
 
@@ -31,15 +17,29 @@ const addTask = () => {
   
 
   if(task){
-  todoTask.innerHTML += `<div class="container-todo__element" id="toodoElement">
+  todoTask.innerHTML += `<div class="container-todo__element" id="todoElement">
   <p class="container-todo__element-text">${task}</p>
   <button class='container-todo__element-undone'>X</button>
   <button class='container-todo__element-done' id="done-button">OK</button>
-</div>`
-}
+</div>`}
   inputTask.value = "";
+ 
+};
+
+function doneTask(e){
+    let element = e.target;
+    if(element.classList.contains("container-todo__element-done")){
+        console.log(element.parentNode.firstChild.innerHTML);
+        
+        element.parentNode.remove();
+    }
+
 }
 
 
 addButton.addEventListener("click", addTask);
-doneButton.addEventListener("click", doneTask);
+
+document.addEventListener( "click", doneTask );
+
+
+
